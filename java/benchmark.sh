326f0cb4f09543a99a38dfe121d72a07d4979822
Fernando Ipar <fipar@acm.org>
2013-04-19 10:09:58 -0300

    changed benchmark script

--- java/benchmark.sh
+++ java/benchmark.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-levels="5 50 100 500"
-
-for noOfDatabases in $levels; do
-    for noOfUsers in $levels; do
-	for noOfTransactions in $levels; do 
-	    for concurrency in $levels; do
-		java -jar target/DistributedTxn-1.0-SNAPSHOT.jar $noOfDatabases $noOfUsers $noOfTransactions $concurrency 5 2>/dev/null
-	    done
-	done
+for noOfDatabases in 2 5 10; do
+    for noOfUsers in 5 50 100 500; do
+        for noOfTransactions in 2 10 50 100 500; do 
+            for concurrency in 1 2 10 25 50 100 150; do
+                echo "starting test with parameters $noOfDatabases $noOfUsers $noOfTransactions $concurrency 5"
+                java -XX:-PrintGC -Xss128k -jar target/DistributedTxn-1.0-SNAPSHOT.jar $noOfDatabases $noOfUsers $noOfTransactions $concurrency 5 2>error.log
+            done
+        done
     done
 done
-
